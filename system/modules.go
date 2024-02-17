ad13e1afc4f7ab7520c1230b54cf9d3faaeab045
MTplusWebSystem <mtpluswebsystem@gmail.com>
2024-02-17 07:14:43 -0400

    Funções iniciais a serem criadas

--- system/modules.go
+++ system/modules.go
@@ -7,3 +7,19 @@ func NilError(msg string , err error) {
 		fmt.Println(msg,err)
 	}
 }
+
+/*
+funções para serem criadas
+
+NilError: simplificar as mensagens de erros e diminuir linhas de código
+
+Scan: para leitura simples de arquivos
+
+Regex: Uma forma simples de aplicar regex
+
+WriteJSON: para escrita de arquivos JSON
+
+Random: para geração de valores aleatórios
+
+KeyGenerator: para geração de chaves aleatórias
+*/
