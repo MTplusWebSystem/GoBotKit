073ae1bab7050d3322466aca415f74ab51fa3213
MTplusWebSystem <mtpluswebsystem@gmail.com>
2024-02-17 09:50:26 -0400

    Começando o pacote requests

---
+++ requests/GET.go
@@ -0,0 +1,10 @@
+package requests
+
+/*
+Pacote requests inicilmente vai estar disponivel GET, POST
+
+funções para requisição GET
+
+GET : para requisição GET
+ReadBody : para ler o corpo da requisição
+*/
