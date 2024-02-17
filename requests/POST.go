72b82d6522e421999a6d7389da43f37ea20bd8ca
MTplusWebSystem <mtpluswebsystem@gmail.com>
2024-02-17 10:02:00 -0400

    Começando POST

---
+++ requests/POST.go
@@ -0,0 +1,11 @@
+package requests
+
+/*
+Pacote requests inicilmente vai estar disponivel GET, POST
+
+funções para requisição POST
+
+POST: para requisição POST
+
+POSTmultipart : para requisição multipart
+*/
