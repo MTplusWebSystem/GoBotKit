101524136af03342f0cb3fc588eb3ae9fa64062c
MTplusWebSystem <mtpluswebsystem@gmail.com>
2024-02-17 09:53:23 -0400

    Requisição GET

--- requests/GET.go
+++ requests/GET.go
@@ -1,5 +1,11 @@
 package requests
 
+import (
+	"net/http"
+
+	"github.com/MTplusWebSystem/GoBotKit/system"
+)
+
 /*
 Pacote requests inicilmente vai estar disponivel GET, POST
 
@@ -8,3 +14,9 @@ funções para requisição GET
 GET : para requisição GET
 ReadBody : para ler o corpo da requisição
 */
+
+func GET(url string) (*http.Response, error){
+	req , err := http.Get(url)
+	system.NilError("Erro na solicitação",err)
+	return req, nil
+}
