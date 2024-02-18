c08d835a7b8807b417dfa9a95264633d42967464
MTplusWebSystem <mtpluswebsystem@gmail.com>
2024-02-18 09:21:06 -0400

    Planejamento inicial

---
+++ botkit/GetUpdates.go
@@ -0,0 +1,37 @@
+package botkit
+
+/*
+Criando as funçoes para criação de bot
+
+GetUpdates
+
+ReceiveData
+
+SendMessage
+
+DeleteMessage
+
+EditMessage -> fica para proxima atualização
+
+SendPhoto
+
+SendAudio -> fica para proxima atualização
+
+SendDocument -> fica para proxima atualização
+
+SendSticker
+
+SendVideo -> fica para proxima atualização
+
+KeybordButton
+
+InlineKeyboardButton
+
+InlineKeyboardMarkup
+
+ReplayButton
+
+ReplayToMessage
+
+ReplayToPhotoButton
+*/
