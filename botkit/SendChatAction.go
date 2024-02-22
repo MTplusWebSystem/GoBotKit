595ca5ed0803d98b302122c92a6bad96d0a564f8
MTplusWebSystem <mtpluswebsystem@gmail.com>
2024-02-22 19:30:30 -0400

    SendDocument

--- botkit/SendChatAction.go
+++ botkit/SendChatAction.go
@@ -7,12 +7,12 @@ import (
 )
 
 func (b *BotInit) SendChatAction() error {
-	UrlAction := fmt.Sprintf("https://api.telegram.org/bot%s/sendChatAction", b.Token)
-    paramsAction := map[string]interface{}{
+	Url := fmt.Sprintf("https://api.telegram.org/bot%s/sendChatAction", b.Token)
+    params := map[string]interface{}{
         "chat_id": b.ChatID,
         "action":  "typing", 
     }
-    requests.POST(UrlAction, "", paramsAction)
+    requests.POST(Url, "", params)
 
 	return nil
 }
