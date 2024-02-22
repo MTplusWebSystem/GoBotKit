cdff122c0e1acaf17b3c25cb9a9e9e6025cb8bc6
MTplusWebSystem <mtpluswebsystem@gmail.com>
2024-02-22 15:44:11 -0400

    ForceReply

---
+++ botkit/ForceReply.go
@@ -0,0 +1,19 @@
+package botkit
+
+import (
+    "fmt"
+    "github.com/MTplusWebSystem/GoBotKit/requests"
+)
+
+func (b *BotInit) ForceReply(messageID int) error {
+    Url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", b.Token)
+    params := map[string]interface{}{
+        "chat_id":            b.ChatID,
+        "reply_to_message_id": messageID,
+        "reply_markup": map[string]interface{}{
+            "force_reply": true,
+        },
+    }
+    requests.POST(Url, "", params)
+    return nil
+}
