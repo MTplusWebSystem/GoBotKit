d81f092f9621ffd0b9083632c1169baec1d02426
MTplusWebSystem <128556678+MTplusWebSystem@users.noreply.github.com>
2024-07-11 13:26:31 -0400

    Update SendMessages.go
    uso de ID do usuário ou grupo e chat direto no SendMessages

--- botkit/SendMessages.go
+++ botkit/SendMessages.go
@@ -5,16 +5,20 @@ import (
 	"github.com/MTplusWebSystem/GoBotKit/requests"
 )
 
-
-
 func (b *BotInit) SendMessages(message string, format ...string) {
     parseMode := "HTML"
+    chatID := b.ChatID
+
     if len(format) > 0 {
         parseMode = format[0]
     }
+    
+    if len(format) > 1 {
+        chatID = format[1]
+    }
 
     params := map[string]interface{}{
-        "chat_id":    b.ChatID,
+        "chat_id":    chatID,
         "parse_mode": parseMode,
         "text":       message,
     }
